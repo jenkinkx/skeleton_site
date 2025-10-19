1166fa460b397d98b46face95bee35997d70ddba
Kendall <kendalljenkins@joinbuzzd.com>
2025-10-19 19:43:28 -0400

    Part 7: Add session-based visit counter to index view and template

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Book, Author, BookInstance, Genre
 
-# Create your views here.
+def index(request):
+    # Number of visits to this view, as counted in the session variable.
+    num_visits = request.session.get('num_visits', 0)
+    num_visits += 1
+    request.session['num_visits'] = num_visits
+
+    context = {
+        'num_books': Book.objects.count(),
+        'num_instances': BookInstance.objects.count(),
+        'num_instances_available': BookInstance.objects.filter(status='a').count(),
+        'num_authors': Author.objects.count(),
+        'num_genres': Genre.objects.count(),
+        'num_visits': num_visits,
+    }
+    return render(request, 'catalog/index.html', context)
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
